ed9ad1eb123e869de06b2fddf9459d163ad2ea34
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-09 17:54:06 +0000

    Adds hard coded data

--- MovieTickets/src/Movies.js
+++ MovieTickets/src/Movies.js
@@ -7,27 +7,8 @@ import {
 } from 'react-native';
 import { movies } from './data';
 
-const DATABASEURL = ' http://127.0.0.1:3000/v1/movies.json'
 
 export default class Movies extends Component {
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      responseData: {}
-    }
-  },
-
-  return fetch(DATABASEURL)
-   .then((response) => response.json())
-   .then((responseJson) => {
-     this.state: responseJson.movies;
-   })
-   .catch((error) => {
-     console.error(error);
-   });
-},
 
   render() {
     return (
@@ -36,6 +17,6 @@ export default class Movies extends Component {
           {movies.map((movie, index) => <Text>{movie.title}</Text>)}
         <ScrollView>
       </View>
-    )
+    );
   }
 }
