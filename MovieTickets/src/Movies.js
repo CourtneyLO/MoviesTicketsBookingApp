2bf7672ff82aea16437ed648424b3fd0af3d9330
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-09 18:25:27 +0000

    renders posters

--- MovieTickets/src/Movies.js
+++ MovieTickets/src/Movies.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-
 import {
   ScrollView,
-  Test,
+  Text,
   View
 } from 'react-native';
 import { movies } from './data';
 
-
 export default class Movies extends Component {
-
   render() {
     return (
       <View>
         <ScrollView>
           {movies.map((movie, index) => <Text>{movie.title}</Text>)}
-        <ScrollView>
+        </ScrollView>
       </View>
     );
   }
--- MovieTickets/src/app.js
+++ MovieTickets/src/app.js
@@ -1,22 +1,21 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react';
 import {
   Navigator,
 } from 'react-native';
-
 import Movies from './Movies';
 
-const RouteMapper = (route, navgiator) => {
-  if (route.name === "Movies") {
-    return <Movies navigator={navigator}>
+const RouteMapper = (route, navigator) => {
+  if (route.name === 'movies') {
+    return <Movies navigator={navigator} />;
   }
 };
 
 export default class App extends Component {
-  render()
+  render() {
     return (
       <Navigator
-        initialRoute={{ name: "Movies" }}
-        configScene={(route, routeScrene) => Navigator.SceneConfigs.FloatFromBottom}
+        initialRoute={{ name: 'movies' }}
+        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
         renderScene={RouteMapper}
       />
     );
